69a413b4a46de3b7fafd1ace57aedfd626fdd434
koka <koka.v@husky.neu.edu>
2022-08-17 13:15:07 +0530

    add live stream post images

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Image = ({image, handleImageRemove = f => f}) => (
-    <img src={image.url} key={image.public_id} alt={image.public_id} style={{height: '500px'}}
+    <img src={image.url} key={image.public_id} alt={image.public_id}
+         style={{height: 'auto', width: "100%", objectFit: "contain"}}
          onClick={() => handleImageRemove(image.public_id)}
     />
 );
