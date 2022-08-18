81132df4dca404a0a0e5705bdd2bc3d38f9078a4
koka <koka.v@husky.neu.edu>
2022-08-18 20:35:14 +0530

    add basic css for post images

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Image = ({image, handleImageRemove = f => f}) => (
-    <img src={image.url} key={image.public_id} alt={image.public_id} style={{maxHeight: '300px'}}
+    <img src={image.url} key={image.public_id} alt={image.public_id}
+         style={{width: '100%', height: 'auto'}}
          onClick={() => handleImageRemove(image.public_id)}
     />
 );
