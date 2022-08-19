92099abfd13ba902b15cb31e8aa79b40365fd0af
koka <koka.v@husky.neu.edu>
2022-08-19 12:46:17 +0530

    try out image modal on livestream page

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Image = ({image, handleImageRemove = f => f}) => (
-    <img src={image.url} key={image.public_id} alt={image.public_id}
-         style={{width: '100%', height: 'auto'}}
-         onClick={() => handleImageRemove(image.public_id)}
-    />
+    <div className="postCard__images__image">
+        <img src={image.url} key={image.public_id} alt={image.public_id}
+             style={{width: '100%'}}
+             onClick={() => handleImageRemove(image.public_id)}
+        />
+    </div>
 );
 
 
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import Image from "./Image";
 import {useNavigate} from "react-router";
+
 
 const PostCard = ({
                       post,
@@ -9,8 +10,21 @@ const PostCard = ({
                       showUpdateButton = false,
                       showDeleteButton = false
                   }) => {
+    const [model, setModel] = useState(false);
+    const [tempImgSrc, setTempImgSrc] = useState('');
     const {_id, images, content, postedBy} = post;
     const navigate = useNavigate();
+
+    const getImg = (imgSrc) => {
+        console.log("jaffa image clicked");
+        setTempImgSrc(imgSrc);
+        setModel(true);
+    }
+
+    const closeModel = () => {
+        setModel(false);
+    }
+
     return (
         <div className="postCard">
             {showPostedBy && (
@@ -19,8 +33,18 @@ const PostCard = ({
             <div onClick={() => navigate(`/post/${_id}`)}>
                 {content}
             </div>
+            <div className={model ? 'model open' : 'model container'}>
+                <img src={tempImgSrc}></img>
+                {/*<i className="far fa-times-circle"></i>*/}
+                <i className="far fa-times-circle" onClick={() => closeModel()}></i>
+            </div>
             <div className="postCard__images">
-                {images.map(i => (<Image key={i._id} image={i}/>))}
+                {images.map(i => (<div className="postCard__images__image">
+                    <img src={i.url} key={i.public_id} alt={i.public_id}
+                         style={{width: '100%'}}
+                         onClick={() => getImg(i.url)}
+                    />
+                </div>))}
             </div>
             {showUpdateButton && (
                 <button className="btn m-2 btn-primary"
