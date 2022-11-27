8d5095c2cbc6c523a18f2bec2579b1525bc31fa7
koka <koka.v@husky.neu.edu>
2022-11-27 16:27:55 +0530

    add text to market health page

--- src/pages/trading/MarketHealth.js
+++ src/pages/trading/MarketHealth.js
@@ -5,11 +5,26 @@ const MarketHealth = () => {
     const navigate = useNavigate();
     return (
         <div className="container tradingPage">
-            <div>
+            <div className="tradingPage__content">
                 Market Health can be determined by using moving averages. Even better indicator would be the behaviour
                 of stocks in your portfolio/watchlist(assuming you did shortlist the very best acting stocks).
             </div>
-            <div>
+            <div className="tradingPage__content">
+                One thing to note is that a market index going uo doesn't mean, the stocks in your watchlist will
+                perform well or vice versa. The probability is higher, that's it. Many market tops consists of very few
+                stocks propping up the index, but most of the stocks will be declining underneath the surface. Keep of
+                the new highs/lows number as it determines how many stocks are making a new high vs how many are making
+                a new low. When there is a consistent trend of new highs, there are breakouts that are working and
+                making new high and it's similar to stocks making new low.
+            </div>
+            <div className="tradingPage__content">
+                So, the ultimate indicator is your portfolio/watchlist. If the stocks in your portfolio are having pivot
+                failures or base failures, the market is suggesting yu something. This is not the right time for buying
+                breakouts or increasing the positions. Scale back when this happens. Conversely, if the stock breakouts
+                are holding up and the stock follows through(means continuing the breakout with minimal pullback or
+                going up again after pullback), it's time to step up the buying/adding to positions.
+            </div>
+            <div className="tradingPage__content">
                 The market conditions based on Moving averages will have 2 parts. Whether the index is above or below
                 the moving average and whether the moving average itself is in an uptrend or an downtrend. In some
                 timelines, Exponential M.A is used and Simple MA for other. This is based on my opinion on which one
@@ -23,6 +38,7 @@ const MarketHealth = () => {
 
                 Long-term: 200 SMA.
             </div>
+
             <div className="tradingPage__navigation">
                 <div className="tradingPage__navigation__prevPage">
                     <div className="buttonJ buttonJ__tertiary buttonJ__rounded buttonJ__small"
--- src/pages/trading/PositionManagement.js
+++ src/pages/trading/PositionManagement.js
@@ -10,22 +10,63 @@ const PositionManagement = () => {
             </div>
             <div className="tradingPage__content">
                 At this point, You have bought the stocks that are in an uptrend and are breaking out from proper bases.
-                What's next? To stay in the position to maximize profits while minimizing risk to your portfolio.
+                What next? To stay in the position or not, maximize profits while minimizing risk to your portfolio.
             </div>
             <div className="tradingPage__content">
-                Managing a position consists of two scenarios.
-                <ul>
-                    <li>
-                        The price moves up. You need to know when to take profits and how much to sell.
+                You can have as few as 4 stocks in your portfolio with a 25% position each. But this requires a
+                technique called Progressive exposure(explained below). The secret to compounding your capital rapidly
+                is to buy stocks that are moving up fast, while at the same time reducing risk to the overall portfolio
+                while increasing your position.
+            </div>
+            <div className="tradingPage__content">
+                I have, and some professionals I know of, who traded even 50% - 100% of their portfolio in a single
+                trade. But this requires an extremely tight stop loss and very liquid stock or index ETF. The overall
+                maximum risk on trade remains at 1%. So on a 10% position, you can have a 10% stop loss to maintain the
+                1% risk on the portfolio. When you have a 50% position, a 2% stop is required, and with 100%, a 1% stop.
+                This is only done in rare cases where a setup offers an asymmetrical risk/reward ratio. Don't ever hold
+                these positions overnight, as gap-downs that are more than your stop-loss occurs and thereby making the
+                stop-loss useless.
+            </div>
+            <div className="tradingPage__content">
+                There is no guarantee that even buying the best stocks with precise timing on breakouts will result in
+                profitable trades. Just that the probability of a profitable trade is higher. There are three scenarios
+                post-buy:
+                <ol>
+                    <li>The stock stays there in a range and does not have much progress.</li>
+                    <li>The stock pulls back after you buy and is at a loss. This scenario requires the knowledge of
+                        whether the action is common or uncommon.
                     </li>
-                    <li>
-                        The stock pulls back after you buy and is at a loss. This scenario requires the knowledge of
-                        When to sell and how much to sell.
+                    <li>The price moves up for the next 2-3 days, and the position is in profit.
                     </li>
-                    <li>
-                        The stocks just stays there in a range and doesn't have much progress.
-                    </li>
-                </ul>
+                </ol>
+            </div>
+            <div className="tradingPage__content">
+                If the stock stays in a range without any progress, you can either wait for it to push higher or reduce
+                the position / sell altogether to buy other stock that is behaving better. You do not have to tie your
+                capital in a stagnant stock when the other are acting well.
+            </div>
+            <div className="tradingPage__content">
+                The stock pulled back: A stock price pulling back after a breakout is a common action. But staying there
+                is not. The price has to rebound immediately to push higher. You can allow some time for the stock to
+                show its potential. If it doesn't rebound, reduce the position(if the stock hits your stop-loss, you get
+                out of the trade immediately anyway).
+            </div>
+            <h1>-------------Image of rebound after squatting -----------------------</h1>
+            <div className="tradingPage__content">
+                To stay in the trade, the stock has to be in profit immediately(price moving up the next 2-3days with
+                increased volume) and without pulling back much. Maximum attention has to be paid to the price action in
+                the first few days. It's better if the price never came back to the breakout level, but it is very
+                common for a stock to revisit the breakout price level and turn up and move higher. You can certainly
+                add to a position if the stock starts turning up after a pull-back.
+            </div>
+            <h1>-------------Image of turning up after pull-back -----------------------</h1>
+            <div className="tradingPage__content">
+                Also as the stock price moves up, you can increase the position size and offset the risk with your
+                profit. Let's say you bought a stock at $100 with 5% of your overall portfolio and it moves up to $106
+                (3% profit), you can bump your position to 10%. Your risk doubled, but the profit offsets it. If the
+                stock turns back, you can decrease the position immediately at the break-even price and your original 5%
+                with initial risk. But if the stock moves up after increasing the position, it will be like doubling the
+                profit potential without additional risk.
             </div>
             <div className="tradingPage__navigation">
                 <div className="tradingPage__navigation__prevPage">
